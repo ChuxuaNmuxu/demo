2bf074acf527aecbfd10eab82840ee3fcfb3ffaa
sunb001 <2267660581@qq.com>
2018-01-18 09:42:36 +0800

    log 函数

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-import './primer';
-// import './applicative';
+// import {curry} from 'lodash';
+
+var LOG_COUNT = 1;
+
+function log (func) {
+    return function (...rest) {
+        console.log(LOG_COUNT++, func.apply(func, rest))
+    }
+}
+
+// var log = curry(consoleLog)
+
+var Container = function (x) {
+    this.__value = x;
+}
+
+// 省去new关键字
+Container.of = function (x) {
+    return new Container(x)
+}
+
+const logContainer = log(Container.of);
+
+logContainer('a');
+log(Container.of)(Container.of('b'));
