e2f9a718b0d0fb8584092635ed6253f4b67d03c9
sunb001 <2267660581@qq.com>
2018-01-19 18:32:09 +0800

    maybe

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-// import {curry} from 'lodash';
+import {
+    curry,
+    isFunction
+} from 'lodash';
 
-var LOG_COUNT = 1;
-
-function log (func) {
-    return function (...rest) {
-        console.log(LOG_COUNT++, func.apply(func, rest))
+const logInitNumber = initCount => {
+    let count = initCount;
+    return func => {
+        if (!isFunction(func)) return console.log(count++, func);
+        return function (...rest) {
+            console.log(count++, func.apply(func, rest))
+        }
     }
 }
 
-// var log = curry(consoleLog)
+const log = logInitNumber(1);
 
-var Container = function (x) {
+const Container = function (x) {
     this.__value = x;
 }
 
-// 省去new关键字
+/**
+ *  1. 省去new关键字
+ *  a -> Container a
+ * @param {*any} x 
+ */
 Container.of = function (x) {
     return new Container(x)
 }
@@ -23,3 +32,14 @@ const logContainer = log(Container.of);
 
 logContainer('a');
 log(Container.of)(Container.of('b'));
+
+// (a -> b) -> Container a -> Container b
+Container.prototype.map = function (f) {
+    return Container.of(f(this.__value))
+}
+
+log(Container.of(2).map(x => x * x))
+
+const Maybe = x => {
+    this.__value = x;
+}
