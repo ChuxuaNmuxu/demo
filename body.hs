d42d46b3b0e52b31796002c497c6dfccba737c0f
sunb001 <2267660581@qq.com>
2018-01-27 17:20:54 +0800

    helloworld

--- body.hs
+++ body.hs
@@ -23,9 +23,22 @@ max' a b
     | a > b     = a
     | otherwise = b
 
-zip' [a] -> [b] -> [(a, b)]
+zip' :: [a] -> [b] -> [(a, b)]
 zip' _ [] = []
 zip' [] _ = []
-zip' (x:xs) (y:ys) = (x y) : zip' xs ys
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
 
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain   n
+        | even n = n : chain(n `div` 2) 
+        | odd n = n : chain(n * 3 + 1)
+
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..10]))
+                where isLong xs = length xs > 15
+
+numLongChains' :: Int
+numLongChains' = length (filter (\xs -> length xs > 15) (map chain [1..100]))
 
