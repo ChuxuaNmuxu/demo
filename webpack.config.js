5db4ebbdd871f925e863a7a0382d77a84e414d95
sunb001 <2267660581@qq.com>
2018-03-26 16:16:33 +0800

    react base starter

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,10 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 // 获取src下的文件目录
-const files = fs.readdirSync(path.join(__dirname, 'src'));
+const entryPath = path.join(__dirname, 'src/entry');
+const files = fs.readdirSync(entryPath);
 const entry = files.reduce((acce, filename) => {
-    acce[path.basename(filename, '.js')] = path.join(__dirname, './src/', filename);
+    acce[path.basename(filename, '.js')] = path.join(entryPath, filename);
     return acce;
 } , {})
 
@@ -30,12 +31,20 @@ module.exports = {
         hot: true // 热加载
     },
 
+    resolve: {
+        alias: {
+            component: path.join(__dirname, 'src/component/')
+        },
+        extensions: ['.js', '.jsx']
+    },
+
     plugins: [
         new HtmlWebpackPlugin({ // 生成html，并自动引入js文件
-            title: 'output management'
+            title: 'output management',
+            template: path.join(__dirname, 'src/layout/layout.html')
         }),
         new CleanWebpackPlugin(['dist']), // 每次打包清理dist文件夹
-        new webpack.HotModuleReplacementPlugin() // 热加载
+        new webpack.HotModuleReplacementPlugin(), // 热加载
         // new UglifyJSPlugin() // tree shaking 不加载没有使用的模块
     ],
 
