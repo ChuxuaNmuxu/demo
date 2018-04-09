dc6d892ed36c1e48a29db0975ca5882096495a7e
sunb001 <2267660581@qq.com>
2018-04-09 17:24:27 +0800

    path-to-regexp 匹配规则测试路由

--- src/entry/app.js
+++ src/entry/app.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from 'component/app';
 
+var pathToRegexp = require('path-to-regexp');
+var re = pathToRegexp('/(editor|editor/.*|courseEntry)?')
+// keys = [{ name: 'foo', prefix: '/', ... }, { name: 'bar', prefix: '/', ... }]
+ 
+const result = re.exec('/edito');
+//=> ['/test/route', 'test', 'route']
+console.log('result: ', result)
+
+
 const root = document.getElementById('root');
 ReactDom.render(<App />, root);
