0d521aa7060ace521012fe738ea7703b252f77e2
sunb001 <2267660581@qq.com>
2018-05-16 23:08:30 +0800

    旋转

--- src/component/app.js
+++ src/component/app.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './app.css'
 
 class App extends Component {
     render() {
         return (
-            <div>
-                App demo !
+            <div className='container' >
+                <div className="demo">
+                    demo
+                </div>
+
+                <div className="children1">
+                    children1
+                </div>
+
+                <div className="children2">
+                    children2
+                </div>
             </div>
         );
     }
